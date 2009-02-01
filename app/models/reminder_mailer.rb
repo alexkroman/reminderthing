dc791bf82675ee06ef1c81ea906c1226533369b7
Alex <alex@alex-desktop.(none)>
2009-02-01 15:23:04 -0800

    change email address

--- app/models/reminder_mailer.rb
+++ app/models/reminder_mailer.rb
@@ -3,7 +3,7 @@ class ReminderMailer < ActionMailer::Base
   def message(message)
     @content_type = "text/plain"
     @recipients = message.email
-    @from = 'sms@alexkroman.com'
+    @from = 'sms@alexkroman.com <ReminderThing>'
     @subject = message.message
   end
 

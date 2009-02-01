31850416bf8d8fe47102b2bd075cf1ba2e5f5e54
Alex <alex@alex-desktop.(none)>
2009-02-01 15:19:31 -0800

    fix mailer

--- app/models/reminder_mailer.rb
+++ app/models/reminder_mailer.rb
@@ -1,15 +1,10 @@
 class ReminderMailer < ActionMailer::Base
 
   def message(message)
-    subject = message.message
-  end
-
-  private 
-  
-  def setup
-    content_type      "text/plain"
-    recipients        message.email
-    from              'sms@texttosms.com'
+    @content_type = "text/plain"
+    @recipients = message.email
+    @from = 'sms@alexkroman.com'
+    @subject = message.message
   end
 
 end

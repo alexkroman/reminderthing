d0ce14a4ad9f8500173af2123a664fa26e3ace2c
Alex <alex@alex-desktop.(none)>
2009-02-01 21:01:01 -0800

    redesign

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
   helper :all # include all helpers, all the time
+  before_filter :find_reminders
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -15,4 +16,22 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  #
+
+  protected
+
+  def find_reminders
+    @reminders = {}
+    if logged_in?
+      @all_reminders = Reminder.find_owned(current_user)
+    else
+      @all_reminders = Reminder.find_guest(session[:csrf_id])
+    end
+
+    emails = @all_reminders.map{|r| r.email}.uniq.sort
+    emails.each do |email|
+      @reminders[email] = @all_reminders.select{|r| r.email == email}.group_by(&:send_at_date_display)
+    end
+  end
+
 end
--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,15 +1,12 @@
 class RemindersController < ApplicationController
   before_filter :set_timezone, :find_reminders, :except => [:send_messages]
 
+  def index
+    redirect_to new_reminder_path if @all_reminders.size == 0
+  end
+
   def new
-    if session[:reminder]
-      @reminder = session[:reminder].clone
-      @reminder.phone_number = session[:reminder].phone_number
-      @reminder.send_at_date = session[:reminder].send_at_date
-      @reminder.send_at_time = session[:reminder].send_at_time
-    else
-      @reminder = Reminder.new
-    end
+    @reminder = Reminder.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -27,12 +24,7 @@ class RemindersController < ApplicationController
 
     respond_to do |format|
       if @reminder.save
-        @reminder.send_at_date = params[:reminder][:send_at_date]
-        @reminder.send_at_time = params[:reminder][:send_at_time]
-        @reminder.phone_number = params[:reminder][:phone_number]
-        session[:reminder] = @reminder
-        flash[:notice] = "Your reminder \"#{@reminder.message}\" was scheduled."
-        format.html { redirect_to(new_reminder_path) }
+        format.html { redirect_to(reminders_path) }
       else
         format.html { render :action => "new" }
       end
@@ -46,7 +38,7 @@ class RemindersController < ApplicationController
       @reminder = Reminder.find_by_id_and_session_id(params[:id], session[:csrf_id])
     end
     @reminder.destroy
-    redirect_to(new_reminder_path)
+    redirect_to(reminders_path)
   end
 
   def send_messages
@@ -63,20 +55,6 @@ class RemindersController < ApplicationController
       session[:time_zone] = params[:reminder][:time_zone].to_i
     end
     Time.zone = 0 - session[:time_zone].to_i if session[:time_zone]
-  end
-
-  def find_reminders
-    @reminders = {}
-    if logged_in?
-      @all_reminders = Reminder.find_owned(current_user)
-    else
-      @all_reminders = Reminder.find_guest(session[:csrf_id])
-    end
-
-    emails = @all_reminders.map{|r| r.email}.uniq.sort
-    emails.each do |email|
-      @reminders[email] = @all_reminders.select{|r| r.email == email}.group_by(&:send_at_date_display)
-    end
   end
 
 end

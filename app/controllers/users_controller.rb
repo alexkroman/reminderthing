3f58dbd4d81df3191ade0b3be9285f2b3980bb5d
Alex <alex@alex-desktop.(none)>
2009-02-02 07:14:12 -0800

    new design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       old_reminders = Reminder.find_all_by_session_id(session[:csrf_id])
       current_user.give_ownership_of(old_reminders)
 
-      redirect_back_or_default('/')
+      redirect_to reminders_path
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."

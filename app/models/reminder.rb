e3cdc37bafa664b2051bbbd7e55ebaa94d06ddc9
Alex <alex@alex-desktop.(none)>
2009-02-04 05:53:44 -0800

    fix date query

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -38,13 +38,13 @@ class Reminder < ActiveRecord::Base
 
   def self.find_guest(session_id)
     Reminder.find(:all,
-                  :conditions => ['send_at >= ? AND session_id = ?', Date.today, session_id],
+                  :conditions => ['send_at >= ? AND session_id = ?', Time.now.midnight, session_id],
                   :order => 'send_at ASC')
   end
 
   def self.find_owned(user)
     Reminder.find(:all,
-                  :conditions => ['send_at >= ? AND user_id = ?', Date.today, user.id],
+                  :conditions => ['send_at >= ? AND user_id = ?', Time.now.midnight, user.id],
                   :order => 'send_at ASC')
   end
 

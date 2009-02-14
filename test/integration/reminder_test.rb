07e7dcb58d90298588b26217e65dcf7155e447a2
Alex <alex@alex-desktop.(none)>
2009-02-14 08:17:43 -0800

    more tests

--- test/integration/reminder_test.rb
+++ test/integration/reminder_test.rb
@@ -6,11 +6,17 @@ class ReminderTest < ActionController::IntegrationTest
   test "post reminder" do
 
     visit root_path
+
+    fill_in "reminder_phone_number", :with => 'test@test.com'
+    fill_in "reminder_message", :with => 'please remind me'
+    fill_in "reminder_send_at_date", :with => 'May 1, 2009'
+    fill_in "reminder_send_at_time", :with => '1:23am'  
+    click_button "Schedule"
+
     click_link "Login"
     fill_in "email", :with => 'quentin@example.com'
     fill_in "password", :with => 'monkey'
     click_button "Login"
-
 
     fill_in "reminder_phone_number", :with => 'test@test.com'
     fill_in "reminder_message", :with => 'please remind me'
